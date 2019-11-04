b70a69171a126b8975d32e0d135545d68fbf7a3b
Attila Zubor <attilazubor@gmail.com>
2019-11-04 11:28:19 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Adventure
+{
+    class Program
+    {
+        static void Main()
+        {
+            String CharacterName = "John Doe";
+
+            Console.WriteLine("Adventure Game!");
+            Console.WriteLine("Please enter your player name: ");
+            Console.ReadLine();
+            Console.WriteLine("Your name is " + CharacterName);
+        }
+    }
+}
